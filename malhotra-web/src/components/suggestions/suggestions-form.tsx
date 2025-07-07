761a413f7d5d81f0e721b238417d894e46be525d
MalhotraIT <mertdemir@malhotracables.com.tr>
2025-07-07 13:27:03 +0300

    Update suggestions-form.tsx

--- malhotra-web/src/components/suggestions/suggestions-form.tsx
+++ malhotra-web/src/components/suggestions/suggestions-form.tsx
@@ -77,7 +77,7 @@ export default function SuggestionsForm({ texts }: SuggestionsFormProps) {
     formData.append("Message", data.Message)
 
     try {
-      const response = await axios.post("https://formspree.io/f/xwpogdoo", formData)
+      const response = await axios.post("https://formspree.io/f/xvgrnjod", formData)
 
       if (response.status === 200) {
         setSubmitResult({
